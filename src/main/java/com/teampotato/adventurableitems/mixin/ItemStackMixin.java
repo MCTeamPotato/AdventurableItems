2fbe2e70330c4de45919440a3a173bc9040b33d7
Kasualix <670067575@qq.com>
2023-12-15 22:00:08 +0800

    1165

--- src/main/java/com/teampotato/adventurableitems/mixin/ItemStackMixin.java
+++ src/main/java/com/teampotato/adventurableitems/mixin/ItemStackMixin.java
@@ -3,14 +3,13 @@ package com.teampotato.adventurableitems.mixin;
 import com.teampotato.adventurableitems.AdventurableItems;
 import com.teampotato.adventurableitems.api.Adventurable;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
+import net.minecraft.tags.TagContainer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.pattern.BlockInWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,9 +24,10 @@ import java.util.function.Function;
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
 
-    @Shadow public abstract boolean hasAdventureModePlaceTagForBlock(Registry<Block> blockRegistry, BlockInWorld block);
 
     @Shadow public abstract Item getItem();
+
+    @Shadow public abstract boolean hasAdventureModePlaceTagForBlock(TagContainer tagContainer, BlockInWorld block);
 
     @Redirect(method = "onItemUse", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Abilities;mayBuild:Z"))
     private boolean adventurableitems$onCheckPlayerMayBuild(Abilities instance) {
@@ -36,7 +36,7 @@ public abstract class ItemStackMixin {
 
     @Inject(method = "onItemUse", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Abilities;mayBuild:Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
     private void adventurableitems$onItemUse(UseOnContext arg, Function<UseOnContext, InteractionResult> callback, CallbackInfoReturnable<InteractionResult> cir, Player player, BlockPos blockPos, BlockInWorld cachedBlockInfo) {
-        if (player != null && !player.getAbilities().mayBuild && !this.hasAdventureModePlaceTagForBlock(arg.getLevel().registryAccess().registryOrThrow(Registry.BLOCK_REGISTRY), cachedBlockInfo)) {
+        if (player != null && !player.abilities.mayBuild && !this.hasAdventureModePlaceTagForBlock(arg.getLevel().getTagManager(), cachedBlockInfo)) {
             if (AdventurableItems.ALLOW_EVERY_ITEM_USING_IN_ADVENTURE_MODE.get()) return;
             if (((Adventurable)this.getItem()).adventurableItems$isAdventurable()) return;
             cir.setReturnValue(InteractionResult.PASS);
